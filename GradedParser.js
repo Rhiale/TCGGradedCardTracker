6f31519a77b79c423194a70ee777aa5f65766b94
Rhiale <quentin.cousin00@gmail.com>
2025-09-07 00:59:17 +0200

    - New search filter - Checkboxes to remove tables - Buttons to check/uncheck all - Sorting options : add alphabetical

--- GradedParser.js
+++ GradedParser.js
@@ -1,5 +1,7 @@
 const l_CardList = ["Pigni", "Burpig", "Inferhog", "Blazoar", "Kidsune", "Bonfiox", "Honobi", "Kyuenbi", "Nanomite", "Decimite", "Meganite", "Giganite", "Sapoling", "Forush", "Timbro", "Mammotree", "Minstar", "Trickstar", "Princestar", "Kingstar", "Shellow", "Clamigo", "Aquariff", "Fistronk", "Wurmgle", "Pupazz", "Mothini", "Royalama", "Nocti", "Lunight", "Vampicant", "Dracunix", "Minotos", "Drilceros", "Grizzaw", "Jelicleen", "Wispo", "Mummog", "Helio", "Pixy", "Flory", "Magnoria", "Werboo", "Flami", "Angez", "Moskit", "Kyrone", "Twofrost", "Threeze", "Hydroid", "Drakon", "Bogon", "Hydron", "Raizon", "Tortugor", "Lupup", "Luphire", "Lucinder", "Lucadence", "Gupi", "Sharfin", "Gilgabass", "Jigajawr", "Batrang", "Dusko", "Wolgin", "Jacktern", "Tetron", "Raxx", "Gannon", "GigatronX", "Clawop", "Clawdos", "Clawaken", "Clawcifear", "Sunflork", "Scarlios", "Scarkgorus", "Crobib", "Crosilisk", "Crorathian", "Nimblis", "Nimboculo", "Nimbustrike", "Esmeri", "Esmerock", "Esmerdios", "Litspire", "Voltrex", "Crablox", "Clawvenger", "Flambrolly", "Lumie", "Seedant", "Budwing", "Buzzeed", "Beakai", "Talontsu", "Talonika", "Talonryu", "Kataryu", "Katengu", "Mufflin", "Muffleur", "Mufflimax", "Anguifish", "Amneshark", "Amnesilla", "Frizard", "Gekoflare", "Terradrakon", "Flamchik", "Pyropeck", "Poseia", "Posteed", "Poseigon", "Poseidrake", "Sludglop", "Sludgetox", "Toxigoop", "Toximuck"];
+const l_SortedCardList = l_CardList.toSorted();
 const l_GhostCardList = ["Blazoar", "Kyuenbi", "Giganite", "Mammotree", "Kingstar", "Fistronk", "Royalama", "Dracunix", "Magnoria", "Hydroid", "Drakon", "Bogon", "Hydron", "Raizon", "Lucadence", "Jigajawr", "Jacktern", "GigatronX", "Clawcifear", "Katengu"];
+const l_SortedGhostCardList = l_GhostCardList.toSorted();
 
 const l_RarityList = ["Basic", "First Edition", "Silver", "Gold", "EX", "Full Art", "Foil Basic", "Foil First Edition", "Foil Silver", "Foil Gold", "Foil EX", "Foil Full Art"];
 const l_GhostRarityList = ["Normal", "Foil"];
@@ -7,9 +9,13 @@ const l_GhostRarityList = ["Normal", "Foil"];
 const l_GhostTypeList = ["Light", "Dark"];
 
 const l_BasicPackList = ["Pigni", "Kidsune", "Nanomite", "Sapoling", "Minstar", "Shellow", "Wurmgle", "Nocti", "Helio", "Werboo", "Flami", "Kyrone", "Lupup", "Gupi", "Batrang", "Tetron", "Clawop", "Sunflork", "Crobib", "Nimblis", "Esmeri", "Seedant", "Mufflin", "Anguifish", "Flamchik", "Poseia", "Sludglop"];
+const l_SortedBasicPackList = l_BasicPackList.toSorted();
 const l_RarePackList = ["Burpig", "Bonfiox", "Decimite", "Forush", "Trickstar", "Clamigo", "Pupazz", "Lunight", "Minotos", "Jelicleen", "Wispo", "Mummog", "Pixy", "Angez", "Twofrost", "Luphire", "Sharfin", "Dusko", "Raxx", "Clawdos", "Scarlios", "Crosilisk", "Nimboculo", "Esmerock", "Crablox", "Lumie", "Budwing", "Beakai", "Muffleur", "Amneshark", "Frizard", "Pyropeck", "Posteed", "Sludgetox"];
+const l_SortedRarePackList = l_RarePackList.toSorted();
 const l_EpicPackList = ["Inferhog", "Honobi", "Meganite", "Timbro", "Princestar", "Aquariff", "Mothini", "Vampicant", "Drilceros", "Grizzaw", "Flory", "Moskit", "Threeze", "Tortugor", "Lucinder", "Gilgabass", "Wolgin", "Gannon", "Clawaken", "Scarkgorus", "Crorathian", "Nimbustrike", "Esmerdios", "Litspire", "Clawvenger", "Buzzeed", "Talontsu", "Talonika", "Kataryu", "Mufflimax", "Amnesilla", "Gekoflare", "Poseigon", "Toxigoop"];
+const l_SortedEpicPackList = l_EpicPackList.toSorted();
 const l_LegendaryPackList = ["Blazoar", "Kyuenbi", "Giganite", "Mammotree", "Kingstar", "Fistronk", "Royalama", "Dracunix", "Magnoria", "Hydroid", "Drakon", "Bogon", "Hydron", "Raizon", "Lucadence", "Jigajawr", "Jacktern", "GigatronX", "Clawcifear", "Voltrex", "Flambrolly", "Talonryu", "Katengu", "Terradrakon", "Poseidrake", "Toximuck"];
+const l_SortedLegendaryPackList = l_LegendaryPackList.toSorted();
 
 //25 : small shelf display
 //26 : big shelf display
@@ -22,14 +28,43 @@ function getGradedCardDict(convertedJson) {
     let l_CurrentDestinyCards = [];
     let l_CurrentGhostCards = [];
 
-    for (let i = 0; i < 12 * l_CardList.length; i++) {
-        const cardName = l_CardList[Math.floor(i / 12)] + " " + l_RarityList[i % 12];
+    let l_CardListToUseForSorting = l_CardList;
+    let l_GhostCardListToUseForSorting = l_GhostCardList;
+    let l_BasicListToUseForSorting = l_BasicPackList;
+    let l_RareListToUseForSorting = l_RarePackList;
+    let l_EpicListToUseForSorting = l_EpicPackList;
+    let l_LegendaryListToUseForSorting = l_LegendaryPackList;
+
+    const sortingType = document.querySelector('input[name="sortingType"]:checked').value;
+
+    if (sortingType == "alpha") {
+        l_CardListToUseForSorting = l_SortedCardList;
+        l_GhostCardListToUseForSorting = l_SortedGhostCardList;
+        l_BasicListToUseForSorting = l_SortedBasicPackList;
+        l_RareListToUseForSorting = l_SortedRarePackList;
+        l_EpicListToUseForSorting = l_SortedEpicPackList;
+        l_LegendaryListToUseForSorting = l_SortedLegendaryPackList;
+    }
+
+    for (let i = 0; i < 12 * l_CardListToUseForSorting.length; i++) {
+        const cardName = l_CardListToUseForSorting[Math.floor(i / 12)] + " " + l_RarityList[i % 12];
         l_CurrentBaseCards.push({ name: cardName, gradedList: new Array(10).fill(0) });
         l_CurrentDestinyCards.push({ name: cardName, gradedList: new Array(10).fill(0) });
     }
 
-    for (let i = 0; i < 4 * l_GhostCardList.length; i++) {
-        const cardName = l_GhostCardList[Math.floor((i / 2) % 20)] + " " + l_GhostRarityList[i % 2] + " " + l_GhostTypeList[Math.floor(i / 40)];
+    for (let i = 0; i < 4 * l_GhostCardListToUseForSorting.length; i++) {
+        
+        let GhostNameIndex = Math.floor((i / 2) % 20);
+        let GhostRarityIndex = i % 2;
+        let GhostTypeIndex = Math.floor(i / 40);
+        
+        if (sortingType == "alpha") {
+            GhostNameIndex = Math.floor(i / 4);
+            GhostRarityIndex = i % 2;
+            GhostTypeIndex = Math.floor((i % 4) / 2);
+        }
+        
+        const cardName = l_GhostCardListToUseForSorting[GhostNameIndex] + " " + l_GhostRarityList[GhostRarityIndex] + " " + l_GhostTypeList[GhostTypeIndex];
         l_CurrentGhostCards.push({ name: cardName, gradedList: new Array(10).fill(0) });
     }
 
@@ -57,7 +92,7 @@ function getGradedCardDict(convertedJson) {
                     let indexToAdd;
                     if (card.expansionType === 2) {
                         listToAdd = l_CurrentGhostCards;
-                        indexToAdd = l_GhostCardList.indexOf(l_CardList[card.monsterType - 1]) * 2 + card.isFoil + card.isDestiny * 40;
+                        indexToAdd = l_GhostCardListToUseForSorting.indexOf(l_CardList[card.monsterType - 1]) * 2 + card.isFoil + card.isDestiny * 40;
                     } else if (card.expansionType === 1) {
                         listToAdd = l_CurrentDestinyCards;
                         indexToAdd = (card.monsterType - 1) * 12 + card.isFoil * 6 + card.borderType;
@@ -128,52 +163,18 @@ function doesCardMatchFilter(cardArray) {
     return true;
 }
 
+function doesNameMatchInput(text) {
+    const inputText = document.getElementById("nameInput").value;
+    return text.toLowerCase().includes(inputText.toLowerCase());
+}
+
+function isExpansionChecked(expansionIndex) {
+    const expansionInputName = "checkExpansion" + expansionIndex;
+    return document.getElementById(expansionInputName).checked;
+}
+
 function createTable(arraysToParse) {
-    const tableDataBase = arraysToParse[0].map(value => {
-        if (doesCardMatchFilter(value)) {
-            var strLine = `<tr><td>${value.name}</td>`
-            var checkLine = "checkGrade1";
-            for (let i = 0; i<10; i++) {
-                checkLine = "checkGrade"+(i+1).toString();
-                if (document.getElementById(checkLine).checked) {
-                    strLine += `<td>${value.gradedList[i].toString()}</td>`;
-                }
-            }
-            strLine += "</tr>";
-            return strLine;
-        };
-    }).join('');
-
-    const tableDataDestiny = arraysToParse[1].map(value => {
-        if (doesCardMatchFilter(value)) {
-            var strLine = `<tr><td>${value.name}</td>`
-            var checkLine = "checkGrade1";
-            for (let i = 0; i<10; i++) {
-                checkLine = "checkGrade"+(i+1).toString();
-                if (document.getElementById(checkLine).checked) {
-                    strLine += `<td>${value.gradedList[i].toString()}</td>`;
-                }
-            }
-            strLine += "</tr>";
-            return strLine;
-        };
-    }).join('');
-
-    const tableDataGhost = arraysToParse[2].map(value => {
-        if (doesCardMatchFilter(value)) {
-            var strLine = `<tr><td>${value.name}</td>`
-            var checkLine = "checkGrade1";
-            for (let i = 0; i<10; i++) {
-                checkLine = "checkGrade"+(i+1).toString();
-                if (document.getElementById(checkLine).checked) {
-                    strLine += `<td>${value.gradedList[i].toString()}</td>`;
-                }
-            }
-            strLine += "</tr>";
-            return strLine;
-        };
-    }).join('');
-
+    
     var dataHeadCommon = '';
     var checkLine = "checkGrade1";
     for (let i = 0; i<10; i++) {
@@ -183,27 +184,103 @@ function createTable(arraysToParse) {
         }
     }
 
-    const dataHeadBase = `<tr><th scope="col">Base Card Name</th>`+dataHeadCommon+"</tr>";
+    if (isExpansionChecked("1")) {
+        const tableDataBase = arraysToParse[0].map(value => {
+            if (doesCardMatchFilter(value) && doesNameMatchInput(value.name.split(' ')[0])) {
+                var strLine = `<tr><td>${value.name}</td>`
+                var checkLine = "checkGrade1";
+                for (let i = 0; i<10; i++) {
+                    checkLine = "checkGrade"+(i+1).toString();
+                    if (document.getElementById(checkLine).checked) {
+                        strLine += `<td>${value.gradedList[i].toString()}</td>`;
+                    }
+                }
+                strLine += "</tr>";
+                return strLine;
+            };
+        }).join('');
 
-    const dataHeadDestiny = `<tr><th scope="col">Destiny Card Name</th>`+dataHeadCommon+"</tr>";
+        const dataHeadBase = `<tr><th scope="col">Base Card Name</th>`+dataHeadCommon+"</tr>";
 
-    const dataHeadGhost = `<tr><th scope="col">Ghost Card Name</th>`+dataHeadCommon+"</tr>";
+        const tableHeadBase = document.querySelector("#tableHeadBase");
+        tableHeadBase.innerHTML = dataHeadBase;
 
-    const tableHeadBase = document.querySelector("#tableHeadBase");
-    tableHeadBase.innerHTML = dataHeadBase;
+        const tableBodyBase = document.querySelector("#tableBodyBase");
+        tableBodyBase.innerHTML = tableDataBase;
+    } else {
+        const tableHeadBase = document.querySelector("#tableHeadBase");
+        tableHeadBase.innerHTML = "";
 
-    const tableHeadDestiny = document.querySelector("#tableHeadDestiny");
-    tableHeadDestiny.innerHTML = dataHeadDestiny;
+        const tableBodyBase = document.querySelector("#tableBodyBase");
+        tableBodyBase.innerHTML = "";
+    }
 
-    const tableHeadGhost = document.querySelector("#tableHeadGhost");
-    tableHeadGhost.innerHTML = dataHeadGhost;
+    if (isExpansionChecked("2")) {
+        const tableDataDestiny = arraysToParse[1].map(value => {
+            if (doesCardMatchFilter(value) && doesNameMatchInput(value.name)) {
+                var strLine = `<tr><td>${value.name}</td>`
+                var checkLine = "checkGrade1";
+                for (let i = 0; i<10; i++) {
+                    checkLine = "checkGrade"+(i+1).toString();
+                    if (document.getElementById(checkLine).checked) {
+                        strLine += `<td>${value.gradedList[i].toString()}</td>`;
+                    }
+                }
+                strLine += "</tr>";
+                return strLine;
+            };
+        }).join('');
 
-    const tableBodyBase = document.querySelector("#tableBodyBase");
-    tableBodyBase.innerHTML = tableDataBase;
+        const dataHeadDestiny = `<tr><th scope="col">Destiny Card Name</th>`+dataHeadCommon+"</tr>";
 
-    const tableBodyDestiny = document.querySelector("#tableBodyDestiny");
-    tableBodyDestiny.innerHTML = tableDataDestiny;
+        const tableHeadDestiny = document.querySelector("#tableHeadDestiny");
+        tableHeadDestiny.innerHTML = dataHeadDestiny;
 
-    const tableBodyGhost = document.querySelector("#tableBodyGhost");
-    tableBodyGhost.innerHTML = tableDataGhost;
+        const tableBodyDestiny = document.querySelector("#tableBodyDestiny");
+        tableBodyDestiny.innerHTML = tableDataDestiny;
+    } else {
+        const tableHeadDestiny = document.querySelector("#tableHeadDestiny");
+        tableHeadDestiny.innerHTML = "";
+
+        const tableBodyDestiny = document.querySelector("#tableBodyDestiny");
+        tableBodyDestiny.innerHTML = "";
+    }
+
+    if (isExpansionChecked("3")) {
+        const tableDataGhost = arraysToParse[2].map(value => {
+            if (doesCardMatchFilter(value) && doesNameMatchInput(value.name)) {
+                var strLine = `<tr><td>${value.name}</td>`
+                var checkLine = "checkGrade1";
+                for (let i = 0; i<10; i++) {
+                    checkLine = "checkGrade"+(i+1).toString();
+                    if (document.getElementById(checkLine).checked) {
+                        strLine += `<td>${value.gradedList[i].toString()}</td>`;
+                    }
+                }
+                strLine += "</tr>";
+                return strLine;
+            };
+        }).join('');
+
+        const dataHeadGhost = `<tr><th scope="col">Ghost Card Name</th>`+dataHeadCommon+"</tr>";
+
+        const tableHeadGhost = document.querySelector("#tableHeadGhost");
+        tableHeadGhost.innerHTML = dataHeadGhost;
+
+        const tableBodyGhost = document.querySelector("#tableBodyGhost");
+        tableBodyGhost.innerHTML = tableDataGhost;
+    } else {
+        const tableHeadGhost = document.querySelector("#tableHeadGhost");
+        tableHeadGhost.innerHTML = "";
+
+        const tableBodyGhost = document.querySelector("#tableBodyGhost");
+        tableBodyGhost.innerHTML = "";
+    }
+}
+
+function checkAll(value) {
+    for (const elt of document.getElementsByName("checkbox")) {
+        elt.checked = value;
+    }
+    convertToTable(document.getElementById("saveFile").files.item(0));
 }
